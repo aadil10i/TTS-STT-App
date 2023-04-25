6cb34d94f56b677a6b9eee77fc5a52794c6d22bb
aadil10i <aadil3003@outlook.com>
2023-04-25 09:29:23 +0400

    update index,js

--- src/index.jsx
+++ src/index.jsx
@@ -9,15 +9,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
-// import React from 'react';
-// import { createRoot } from 'react-dom/client';
-// import App from './app';
-// import './styles/index.css';
-
-// const root = document.getElementById('root');
-// createRoot(root).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// );
